ca4c32eb036f9fd80dd39c3a86d11bd978c7371d
willi <will.ingarfield@gmail.com>
2018-07-12 13:59:19 -0500

    include -> require

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ IP Address: <input type="text" name="ip"><br>
 </form>
 
 <?php
-include("cmx_util.php");
+require("cmx_util.php");
 if($_POST) {
     $req = new CMXRequest("config.json", $_POST["ip"]);
     echo "<pre>" . json_encode($req->getResponse(), JSON_PRETTY_PRINT) . "</pre>";
